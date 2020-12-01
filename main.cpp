6173795986d4ab4ed9e6b3eb97d21eec9f6e6866
Francisco Mejia <francisco@creacode.tech>
2020-12-01 18:28:38 -0500

    OMP First try

--- main.cpp
+++ main.cpp
@@ -1,108 +1,163 @@
 #include <iostream>
-using namespace std;
 #include <limits>
-//Esto está así por si necesitamos modificar las dimensiones en caso el 
+#include <omp.h>
+#include <iomanip>
+
+using namespace std;
+
+//Esto está así por si necesitamos modificar las dimensiones en caso el
 //speed up no se note tan bien.
 typedef int DIM1;
 typedef int DIM2;
 DIM2 MAX_DIM2 = numeric_limits<DIM2>::max();
 
-int main() {
-
+int main()
+{
     //Listas enlazadas. El grafo va a tener DEMASIADOS infinitos si no.
 
     DIM1 n;
     DIM2 temp;
     cin >> n;
-    DIM2 **matriz = new DIM2*[n];
+    DIM2 **matriz = new DIM2 *[n];
 
-    for (DIM1 i = 0; i < n; i++) {
+    omp_set_num_threads(6);
+
+    double t1 = omp_get_wtime();
+
+    DIM1 i;
+
+    for (i = 0; i < n; i++)
+    {
         matriz[i] = new DIM2[n];
-        for (DIM1 j = 0; j < n; j++) {
+        for (DIM1 j = 0; j < n; j++)
+        {
             cin >> temp;
-            if (temp != -1) matriz[i][j] = temp;
-            else matriz[i][j] = MAX_DIM2;
+            if (temp != -1)
+                matriz[i][j] = temp;
+            else
+                matriz[i][j] = MAX_DIM2;
         }
     }
-    
+
     DIM2 min;
     DIM2 reduced_counter = 0;
 
     // Get min value in each row and substract
 
-    for (DIM1 i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         min = MAX_DIM2;
         //GET MIN VALUE
-        for (DIM1 j = 0; j < n; j++) {
-            if (min > matriz[i][j]) min = matriz[i][j];
+        for (DIM1 j = 0; j < n; j++)
+        {
+            if (min > matriz[i][j])
+            {
+                min = matriz[i][j];
+            }
         }
         //SUBSTRACT MIN VALUE
-        if (min != 0 && min != MAX_DIM2) {
-        for (DIM1 j = 0; j < n; j++) {
-            if (matriz[i][j] != MAX_DIM2) matriz[i][j] -= min;
-        }
-        reduced_counter += min;
+        if (min != 0 && min != MAX_DIM2)
+        {
+            #pragma omp parallel for shared(matriz)
+            for (DIM1 j = 0; j < n; j++)
+            {
+                if (matriz[i][j] != MAX_DIM2)
+                {
+                    matriz[i][j] -= min;
+                }
+            }
+            reduced_counter += min;
         }
     }
 
     // Get min value in each column and substract
-    for (DIM1 j = 0; j < n; j++) {
+    for (DIM1 j = 0; j < n; j++)
+    {
         min = MAX_DIM2;
         //GET MIN VALUE
-        for (DIM1 i = 0; i < n; i++) {
-            if (min > matriz[i][j]) min = matriz[i][j];
+        for (DIM1 i = 0; i < n; i++)
+        {
+            if (min > matriz[i][j])
+            {
+                min = matriz[i][j];
+            }
         }
         //SUBSTRACT MIN VALUE
-        if (min != 0 && min != MAX_DIM2) {
-        for (DIM1 i = 0; i < n; i++) {
-            if (matriz[i][j] != MAX_DIM2) matriz[i][j] -= min;
-        }
-        reduced_counter += min;
+        if (min != 0 && min != MAX_DIM2)
+        {
+            #pragma omp parallel for shared(matriz)
+            for (DIM1 i = 0; i < n; i++)
+            {
+                if (matriz[i][j] != MAX_DIM2)
+                {
+                    matriz[i][j] -= min;
+                }
+            }
+            reduced_counter += min;
         }
     }
 
-    cout << endl << "RC: " << reduced_counter << endl;
+
 
     // From the reduced matrix, check path a -> b by hiding row a and column b
     // This is performed by updating a and b to inf.
     // In this code, just ignore them. You also need to ignore the backwards path: b->a
 
     DIM2 new_reduced_counter = 0;
-    DIM1 h_r = 0, h_c = 1; 
+    DIM1 h_r = 0, h_c = 1;
 
     // Get min value in each row and substract
 
-    for (DIM1 i = 0; i < n; i++) {
+    for (DIM1 i = 0; i < n; i++)
+    {
         min = MAX_DIM2;
         //GET MIN VALUE
-        for (DIM1 j = 0; j < n; j++) {
-            if (i != h_r && j != h_c && min > matriz[i][j]) min = matriz[i][j];
+        for (DIM1 j = 0; j < n; j++)
+        {
+            if (i != h_r && j != h_c && min > matriz[i][j])
+                min = matriz[i][j];
         }
         //SUBSTRACT MIN VALUE
-        if (min != 0 && min != MAX_DIM2) {
-        for (DIM1 j = 0; j < n; j++) {
-            if (matriz[i][j] != MAX_DIM2) matriz[i][j] -= min;
-        }
-        new_reduced_counter += min;
+        if (min != 0 && min != MAX_DIM2)
+        {
+            #pragma omp parallel for shared(matriz)
+            for (DIM1 j = 0; j < n; j++)
+            {
+                if (matriz[i][j] != MAX_DIM2)
+                    matriz[i][j] -= min;
+            }
+            new_reduced_counter += min;
         }
     }
 
     // Get min value in each column and substract
-    for (DIM1 j = 0; j < n; j++) {
+    for (DIM1 j = 0; j < n; j++)
+    {
         min = MAX_DIM2;
         //GET MIN VALUE
-        for (DIM1 i = 0; i < n; i++) {
-            if (i != h_r && j != h_c && min > matriz[i][j]) min = matriz[i][j];
+        for (DIM1 i = 0; i < n; i++)
+        {
+            if (i != h_r && j != h_c && min > matriz[i][j])
+                min = matriz[i][j];
         }
         //SUBSTRACT MIN VALUE
-        if (min != 0 && min != MAX_DIM2) {
-        for (DIM1 i = 0; i < n; i++) {
-            if (matriz[i][j] != MAX_DIM2) matriz[i][j] -= min;
-        }
-        new_reduced_counter += min;
+        if (min != 0 && min != MAX_DIM2)
+        {
+            #pragma omp parallel for shared(matriz)
+            for (DIM1 i = 0; i < n; i++)
+            {
+                if (matriz[i][j] != MAX_DIM2)
+                    matriz[i][j] -= min;
+            }
+            new_reduced_counter += min;
         }
     }
 
-    cout << endl << "NRC: " << new_reduced_counter << endl;
+    double t2 = omp_get_wtime();
 
+    cout << "RC: " << reduced_counter << endl;
+
+    cout << "NRC: " << new_reduced_counter << endl;
+
+    cout << setprecision(8) << "Time: " << t2 - t1 << endl;
 }
