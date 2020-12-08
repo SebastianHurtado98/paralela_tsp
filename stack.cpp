af1262928b36ef654682074a7d8b7178ee7b23f0
Francisco Mejia <francisco@creacode.tech>
2020-12-08 18:49:29 -0500

    stack

--- stack.cpp
+++ stack.cpp
@@ -7,8 +7,11 @@ using namespace std;
 int main()
 {
     int n;
-    int *matrix, *original_matrix;
+    int root;
+    int *matrix, *row;
     int *stack;
+    int *queue;
+    int *visited;
 
     int rank;
     int size;
@@ -19,11 +22,10 @@ int main()
 
     if (rank == 0)
     {
-        ifstream ifs("input.txt");
-        ifs >> n;
+        int n;
+        cin >> n;
 
         stack = (int *)malloc(n * sizeof(int));
-
         matrix = (int *)malloc(n * n * sizeof(int));
 
         for (int i = 0; i < n; i++)
@@ -31,7 +33,7 @@ int main()
             for (int j = 0; j < n; j++)
             {
                 int temp;
-                ifs >> temp;
+                cin >> temp;
                 if (temp != -1)
                 {
                     matrix[(i * n) + j] = temp;
@@ -43,22 +45,76 @@ int main()
             }
         }
 
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                cout << matrix[(i * n) + j] << " ";
-            }
-            cout << "\n";
-        }
+        cin >> root;
     }
 
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&root, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    if (rank != 0)
+    row = (int *)malloc(n * sizeof(int));
+    queue = (int *)malloc(n * sizeof(int));
+    visited = (int *)malloc(n * sizeof(int));
+
+    MPI_Scatter(matrix, n, MPI_INT, row, n, MPI_INT, 0, MPI_COMM_WORLD);
+
+    for (int i = 0; i < n; i++)
     {
-        matrix = (int *)malloc(n * n * sizeof(int));
+        queue[i] = -1;
     }
 
-    MPI_Bcast(matrix, n * n, MPI_INT, 0, MPI_COMM_WORLD);
+    int idx = 0;
+    if (rank >= root)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (row[i] >= 1)
+            {
+                queue[idx++] = i;
+            }
+        }
+    }
+
+    cout << "Process " << rank << ": ";
+    for (int i = 0; i < idx; i++)
+    {
+        cout << queue[i] << " ";
+    }
+    cout << endl;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    MPI_Gather(queue, n, MPI + INT, bfs_traversal, n, MPI_INT, 0, MPI_COMM_WORLD);
+
+    for (int i = 0; i < n; i++)
+    {
+        visited[i] = 0;
+    }
+
+    if (rank == 0)
+    {
+        cout << "BFS:\n";
+        cout << root;
+        for (int i = 0; i < n * n; i++)
+        {
+            if (allVisited(visited, n))
+            {
+                break;
+            }
+
+            if (bfs_traversal[i] != -1)
+            {
+                if (visited[bfs_traversal[i]] == 0)
+                {
+                    cout << " -> " << bfs_traversal[i];
+                    visited[bfs_traversal[i]] = 1;
+                }
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+
+    MPI_Finalize();
 }
